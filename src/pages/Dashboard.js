e182442ebc4eaa040d1ea71a93762adb3de9cd49
Sangenis Pablo <sangenispablo@gmail.com>
2022-04-24 11:28:41 -0300

    Prueba con fetch en Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const fecthData = async () => {
+    try {
+      const response = await fetch("http://localhost:4000");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fecthData();
+  }, []);
   return <h1>Dashboard</h1>;
 };
 export default Dashboard;
