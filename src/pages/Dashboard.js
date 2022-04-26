a6ee986d8c27c01ce73d7148dc4246c5d6e6a8cf
Sangenis Pablo <sangenispablo@gmail.com>
2022-04-25 23:41:29 -0300

    Probando fetch con y sin cors

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,22 @@
 import { useEffect } from "react";
 
 const Dashboard = () => {
+
   const fecthData = async () => {
     try {
-      const response = await fetch("http://localhost:4000");
+      // const response = await fetch("/data.json");
+      const response = await fetch("/api/v1");
       const data = await response.json();
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fecthData();
   }, []);
+
   return <h1>Dashboard</h1>;
 };
 export default Dashboard;
