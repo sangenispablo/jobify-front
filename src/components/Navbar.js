5ec23c400b7d7b021ea9770af4ee26eb533984ef
Sangenis Pablo <sangenispablo@gmail.com>
2022-04-30 23:57:53 -0300

    dashboard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,35 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import { Logo } from "../components";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => {
+            console.log("toggle sidebar");
+          }}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              console.log("show/hide dropdown");
+            }}
+          ></button>
+        </div>
+      </div>
     </Wrapper>
   );
 };
