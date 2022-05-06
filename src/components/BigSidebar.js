e89c2e32e38bfa983929df820708b8a508e7f5c2
Sangenis Pablo <sangenispablo@gmail.com>
2022-05-05 21:59:12 -0300

    Front basic completed

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,9 +1,24 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSideBar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Big Sidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
